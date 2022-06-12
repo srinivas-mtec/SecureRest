1f0d57fe9f3aa5e611c6f12d2c1107e80cf3308c
srini <srinivas.mtec@gmail.com>
2022-06-12 15:54:54 +0100

    Initial commit

---
+++ SecureRest/src/main/java/com/example/demo/SecureRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureRestApplication.class, args);
+	}
+
+}
